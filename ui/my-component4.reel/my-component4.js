d6367a9113b8abc644bc84a44c07b4e35090481b
Afonso <noreply>
2013-11-17 00:09:14 -0800

    Add component my-component4

---
+++ ui/my-component4.reel/my-component4.js
@@ -0,0 +1,17 @@
+/**
+ * @module ui/my-component4.reel
+ * @requires montage/ui/component
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class MyComponent4
+ * @extends Component
+ */
+exports.MyComponent4 = Component.specialize(/** @lends MyComponent4# */ {
+    constructor: {
+        value: function MyComponent4() {
+            this.super();
+        }
+    }
+});
